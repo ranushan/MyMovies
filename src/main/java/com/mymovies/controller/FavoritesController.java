74250f612b1eea0db0dd20358eb2cfb06583e3bc
Ranushan <ranusha.n@hotmail.fr>
2019-10-23 14:40:33 +0200

    [FEATURE][MANAGE][RATED][UPDATED] remove unused import

--- src/main/java/com/mymovies/controller/FavoritesController.java
+++ src/main/java/com/mymovies/controller/FavoritesController.java
@@ -1,7 +1,5 @@
 package com.mymovies.controller;
 
-import java.io.IOException;
-import java.util.ArrayList;
 import java.util.List;
 
 import org.springframework.beans.factory.annotation.Autowired;
@@ -11,9 +9,7 @@ import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.ResponseBody;
 
-import com.fasterxml.jackson.databind.ObjectMapper;
 import com.mymovies.dto.MovieDTO;
-import com.mymovies.entity.Movie;
 import com.mymovies.entity.User;
 import com.mymovies.service.FavoriteService;
 import com.mymovies.service.UserService;
