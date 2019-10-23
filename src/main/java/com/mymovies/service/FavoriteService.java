1cfebddb770424b4c05ff63893b5a9cfba0e8b42
Ranushan <ranusha.n@hotmail.fr>
2019-10-23 13:07:00 +0200

    [FEATURE][MANAGE][FAVORITE][UPDATED] add 2 methods setFavorite and getFavorites

--- src/main/java/com/mymovies/service/FavoriteService.java
+++ src/main/java/com/mymovies/service/FavoriteService.java
@@ -1,11 +1,17 @@
 package com.mymovies.service;
 
 import java.util.ArrayList;
+import java.util.List;
+
+import com.mymovies.dto.MovieDTO;
+import com.mymovies.entity.User;
 
 public interface FavoriteService {
 	
 	public void addMovieToFavorite(long userId, String movieId);
 	public void removeMovieToFavorite(long userId,String movieId);
 	public ArrayList<String> getFavoriteMoviesListFromUser(long userId);
+	public User setFavorite(int id);
+	public List<MovieDTO> getFavorites();
 	
 }
