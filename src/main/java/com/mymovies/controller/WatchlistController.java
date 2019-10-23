e4814569e24ba19f19002fcaa118fdc802ffe3e6
Ranushan <ranusha.n@hotmail.fr>
2019-10-23 14:03:00 +0200

    [FEATURE][MANAGE][WATCHLIST][UPDATED] call service method

--- src/main/java/com/mymovies/controller/WatchlistController.java
+++ src/main/java/com/mymovies/controller/WatchlistController.java
@@ -1,7 +1,5 @@
 package com.mymovies.controller;
 
-import java.io.IOException;
-import java.util.ArrayList;
 import java.util.List;
 
 import org.springframework.beans.factory.annotation.Autowired;
@@ -11,11 +9,10 @@ import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.ResponseBody;
 
-import com.fasterxml.jackson.databind.ObjectMapper;
 import com.mymovies.dto.MovieDTO;
-import com.mymovies.entity.Movie;
 import com.mymovies.entity.User;
 import com.mymovies.service.UserService;
+import com.mymovies.service.WatchListService;
 
 @Controller
 public class WatchlistController {
@@ -23,39 +20,24 @@ public class WatchlistController {
 	@Autowired
 	UserService userService;
 	
+	@Autowired
+	WatchListService watchlistService;
+	
 	@RequestMapping(value = "/watchlists", method = RequestMethod.GET, produces = "application/json")
 	@ResponseBody
-	public List<MovieDTO> getWatchlists() throws IOException {
+	public List<MovieDTO> getWatchlists() {
 		
-		User ConnectedUser = userService.getAllUsers().get(0);
-		List<Movie> listMovies = ConnectedUser.getWatchlistMovie();
-		
-		MovieController mc = new MovieController();
-
-		List<MovieDTO> listMovieDTO = new ArrayList<>();
-		
-		for(Movie m : listMovies) {
-			listMovieDTO.add(mc.getAPI_Detail(String.valueOf(m.getId())));
-		}
-		
-		return listMovieDTO;
+		List<MovieDTO> listOfWatchlists = watchlistService.getWatchlists();
+		return listOfWatchlists;
 		
 	}
 	
 	@RequestMapping(value = "/watchlists/{idMovie}", method = RequestMethod.POST, produces = "application/json")
 	@ResponseBody
-	public User setWatchlists(@PathVariable("idMovie") int id) throws IOException {
+	public User setWatchlists(@PathVariable("idMovie") int id) {
 
-		MovieDTO fromApi = new MovieDTO();
-		fromApi.setId(id);		
-		ObjectMapper om = new ObjectMapper();
-		String jsonMovie = om.writeValueAsString(fromApi);
-		Movie movie = om.readerFor(Movie.class).readValue(jsonMovie);
-		User ConnectedUser = userService.getAllUsers().get(0);
-		ConnectedUser.addWatchlistMovie(movie);
-		userService.updateUser(ConnectedUser);
-		
-		return ConnectedUser;
+		User watchlist = watchlistService.setWatchlists(id);
+		return watchlist;
 		
 	}
 
