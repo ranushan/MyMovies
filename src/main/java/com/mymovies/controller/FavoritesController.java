a7145281049a85aef2b083c76b85dc9ccdb77d50
Ranushan <ranusha.n@hotmail.fr>
2019-10-23 13:02:26 +0200

    [FEATURE][MANAGE][FAVORITE][UPDATED] remove unused import and call Favorite service

--- src/main/java/com/mymovies/controller/FavoritesController.java
+++ src/main/java/com/mymovies/controller/FavoritesController.java
@@ -1,7 +1,6 @@
 package com.mymovies.controller;
 
 import java.io.IOException;
-import java.util.ArrayList;
 import java.util.List;
 
 import org.springframework.beans.factory.annotation.Autowired;
@@ -10,73 +9,34 @@ import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.bind.annotation.ResponseBody;
-import org.springframework.web.client.RestTemplate;
 
-import com.fasterxml.jackson.databind.ObjectMapper;
 import com.mymovies.dto.MovieDTO;
-import com.mymovies.entity.Favorite;
-import com.mymovies.entity.Movie;
 import com.mymovies.entity.User;
-import com.mymovies.service.UserService;
+import com.mymovies.service.FavoriteService;
 
 
 @Controller
 public class FavoritesController {
 	
 	@Autowired
-	UserService userService;
+	FavoriteService favoriteService;
 	
 	@RequestMapping(value = "/favorites", method = RequestMethod.GET, produces = "application/json")
 	@ResponseBody
-	public List<MovieDTO> getFavorites() throws IOException {
+	public List<MovieDTO> getFavorites() {
 		
-		/*
-		System.out.println("test Jakson ........");
-		MovieDTO m = new MovieDTO();
-		// m.setAdult(true);
-		// m.setOriginal_title("Le scielence des agneaux");
-
-		String jsonMovie = new ObjectMapper().writeValueAsString(m);
+		List<MovieDTO> listOfFavorites = favoriteService.getFavorites();
+		return listOfFavorites;
 		
-		ObjectMapper mapper = new ObjectMapper();
-		MovieDTO movie = mapper.readValue(jsonMovie, MovieDTO.class);
-		MovieDTO movie2 = mapper.readerFor(MovieDTO.class).readValue(jsonMovie);
-		
-		User ConnectedUser = userService.getAllUsers().get(0);
-		return ConnectedUser;
-		
-		*/
-
-		User ConnectedUser = userService.getAllUsers().get(0);
-		List<Movie> listMovies = ConnectedUser.getFavoriteMovie();
-		
-		MovieController mc = new MovieController();
-
-		List<MovieDTO> listMovieDTO = new ArrayList<>();
-		
-		for(Movie m : listMovies) {
-			listMovieDTO.add(mc.getAPI_Detail(String.valueOf(m.getId())));
-		}
-		
-		return listMovieDTO;
-
 	}
 
 	
 	@RequestMapping(value = "/favorites/{idMovie}", method = RequestMethod.POST, produces = "application/json")
 	@ResponseBody
-	public User setFavorites(@PathVariable("idMovie") int id) throws IOException {
-
-		MovieDTO fromApi = new MovieDTO();
-		fromApi.setId(id);		
-		ObjectMapper om = new ObjectMapper();
-		String jsonMovie = om.writeValueAsString(fromApi);
-		Movie movie = om.readerFor(Movie.class).readValue(jsonMovie);
-		User ConnectedUser = userService.getAllUsers().get(0);
-		ConnectedUser.addFavoriteMovie(movie);
-		userService.updateUser(ConnectedUser);
+	public User setFavorites(@PathVariable("idMovie") int id) {
 		
-		return ConnectedUser;
+		User favorite = favoriteService.setFavorite(id);
+		return favorite;
 		
 	}
 	
