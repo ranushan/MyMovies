38c56a8776e52758a1eb819200da135c30249860
Ranushan <ranusha.n@hotmail.fr>
2019-10-23 14:47:03 +0200

    [FEATURE][MANAGE][RATED][UPDATED] add 2 new methods getRates and setRated

--- src/main/java/com/mymovies/service/RatedService.java
+++ src/main/java/com/mymovies/service/RatedService.java
@@ -1,12 +1,18 @@
 package com.mymovies.service;
 
 import java.util.ArrayList;
+import java.util.List;
+
+import com.mymovies.dto.MovieDTO;
+import com.mymovies.entity.User;
 
 public interface RatedService {
 	
 	public void addRatedToMovie(long userId,String movieId);
 	public void removeRatedToMovie(long userId,String movieId);
 	public ArrayList<String> getRatedMoviesListFromUser(long userId);
-
+	public User setRated(int id);
+	public List<MovieDTO> getRates();
+	
 }
 
