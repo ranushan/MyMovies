3f8b1b80064815c4c29799e3f2608ea677a5f536
Ranushan <ranusha.n@hotmail.fr>
2019-10-23 14:06:07 +0200

    [FEATURE][MANAGE][WATCHLIST][UPDATED] add 2 methods getwatchlists and setwatchlist and implement

--- src/main/java/com/mymovies/service/WatchListServiceImpl.java
+++ src/main/java/com/mymovies/service/WatchListServiceImpl.java
@@ -1,17 +1,26 @@
 package com.mymovies.service;
 
 import java.util.ArrayList;
+import java.util.List;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
+import com.fasterxml.jackson.databind.ObjectMapper;
+import com.mymovies.controller.MovieController;
 import com.mymovies.dao.WatchListRepository;
+import com.mymovies.dto.MovieDTO;
+import com.mymovies.entity.Movie;
+import com.mymovies.entity.User;
 
 @Service
 public class WatchListServiceImpl implements WatchListService{
 
 	@Autowired
 	WatchListRepository watchListRepository;
+	
+	@Autowired
+	UserService userService;
 	
 	@Override
 	public void addWatchListToMovie(long userId, String movieId) {
@@ -27,6 +36,54 @@ public class WatchListServiceImpl implements WatchListService{
 	public ArrayList<String> getWatchListMoviesListFromUser(long userId) {
 		ArrayList<String> listOfWatchListMovieListFromUser = watchListRepository.getWatchListMoviesListFromUser(userId);
 		return listOfWatchListMovieListFromUser;
+	}
+
+	@Override
+	public List<MovieDTO> getWatchlists() {
+		
+		List<MovieDTO> listMovieDTO = new ArrayList<>();
+		
+		try {
+			
+			User ConnectedUser = userService.getAllUsers().get(0);
+			List<Movie> listMovies = ConnectedUser.getWatchlistMovie();
+			
+			MovieController mc = new MovieController();
+			
+			for(Movie m : listMovies) {
+				listMovieDTO.add(mc.getAPI_Detail(String.valueOf(m.getId())));
+			}
+	
+		} catch (Exception e) {
+			System.out.println("Exception : " + e.getMessage());
+		}
+		
+		return listMovieDTO;
+		
+	}
+
+	@Override
+	public User setWatchlists(int id) {
+		
+		User ConnectedUser = null;
+		
+		try {
+			
+			MovieDTO fromApi = new MovieDTO();
+			fromApi.setId(id);		
+			ObjectMapper om = new ObjectMapper();
+			String jsonMovie = om.writeValueAsString(fromApi);
+			Movie movie = om.readerFor(Movie.class).readValue(jsonMovie);
+			ConnectedUser = userService.getAllUsers().get(0);
+			ConnectedUser.addWatchlistMovie(movie);
+			userService.updateUser(ConnectedUser);
+			
+		} catch (Exception e) {
+			System.out.println("Exception : " + e.getMessage());
+		}
+		
+		return ConnectedUser;
+		
 	}
 
 }
