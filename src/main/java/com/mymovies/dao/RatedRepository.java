6801694f90034e84bb349eaed9afd6745814d76b
Ranushan <ranusha.n@hotmail.fr>
2019-10-24 20:43:30 +0200

    [FEATURE][MANAGED][RATED][UPDATED] add parameter for addRatedToMovie

--- src/main/java/com/mymovies/dao/RatedRepository.java
+++ src/main/java/com/mymovies/dao/RatedRepository.java
@@ -7,7 +7,7 @@ import javax.persistence.EntityManager;
 public interface RatedRepository {
 
 	public ArrayList<String> getRatedMoviesListFromUser(long userId);
-	public void addRatedToMovie(long userId,String movieId);
+	public void addRatedToMovie(long userId,String movieId, int rate);
 	public void removeRatedToMovie(long userId,String movieId);
 	public EntityManager getEm();
 	
